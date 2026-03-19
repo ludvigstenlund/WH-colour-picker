772c7cf5ab981bf60ee3fb4f1a42e70f04e1070b
Ludvig Stenlund <luddest8@gmail.com>
2026-03-19 13:40:48 +0100

    Motgång med API, jag ska självhosta API med github pages

---
+++ script.js
@@ -0,0 +1,15 @@
+const paintapi = 'https://potterapi-fedeperin.vercel.app'
+
+fetch(paintapi)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
