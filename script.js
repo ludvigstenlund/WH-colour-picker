cf09e20fa58767b612c11bb8ed6d08b0f58b7191
Ludvig Stenlund <luddest8@gmail.com>
2026-04-16 13:43:48 +0200

    sorterat resultat och samlat dessa i en funktion

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const paintapi = 'https://ludvigstenlund.github.io/WH-colour-API/WH-colour-fullrange'
-let resultsdiv = document.getElementById("searchresults");
 
 //knappar
 let base_button = document.getElementById("base")
@@ -10,9 +9,7 @@ let edge_button = document.getElementById("edge")
 let technical_button = document.getElementById("technical")
 let texture_button = document.getElementById("texture")
 let contrast_button = document.getElementById("contrast")
-let filterby = document.getElementById("filter")
-let render_button = document.getElementById("render")
-
+let render_button = document.getElementById("render") 
 
 let showbase = false
 let showlayer = false
@@ -22,9 +19,6 @@ let showedge = false
 let showtechnical = false
 let showtexture = false
 let showcontrast = false
-let filterbytype = true
-let filterbycolor = false
-let togglebycolor = false
 
 base_button.onclick = togglebase
 layer_button.onclick = togglelayer
@@ -34,10 +28,11 @@ edge_button.onclick = toggleedge
 technical_button.onclick = toggletechnical
 texture_button.onclick = toggletexture
 contrast_button.onclick = togglecontrast
-filterby.onclick = filtertoggle
-render_button.onclick = render
+render_button.onclick = getpaints
 
-function togglebase () {showbase = !showbase}
+function togglebase () {showbase = !showbase
+  console.log(showbase)
+}
 function togglelayer () {showlayer = !showlayer}
 function toggleshade () {showshade = !showshade}
 function toggledry () {showdry = !showdry}
@@ -45,72 +40,56 @@ function toggleedge () {showedge = !showedge}
 function toggletechnical () {showtechnical = !showtechnical}
 function toggletexture () {showtexture = !showtexture}
 function togglecontrast () {showcontrast = !showcontrast}
-function filtertoggle () {
-  filterbytype = !filterbytype
-  filterbycolor = !filterbycolor
-  if (filterbytype) {console.log("now filtering by type")}
-  else console.log("now filtering by color")
+
+function createpaintdiv (name, type, group) {
+  let div = document.createElement('div');
+  div.classList.add('aResult');
+  div.classList.add(group)
+  let text = document.createTextNode(name + "\n\n type: " + type + "\n color: " + group);
+  div.appendChild(text);
+  document.getElementById("results").appendChild(div);
 }
-function render () {getpaints()}
 
 function getpaints () {
-  fetch (paintapi) 
+  document.getElementById("results").innerHTML = "" //tömmer diven "results" när getpaints kallas på
+  fetch (paintapi) //hänmtar paintapi
+
   .then(response => {
-    
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
-    return response.json();
-
+    return response.json()
   })
+
   .then(data => {
     console.log(data);
-    output = ""
 
     for (let i = 0; i < data.payload.length; i++) {
-      text = ""
+
       if (showbase === true && data.payload[i].type === "base") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
 
       else if (showlayer === true && data.payload[i].type === "layer") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
 
       else if (showshade === true && data.payload[i].type === "shade") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
+
       else if (showdry === true && data.payload[i].type === "dry") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
+
       else if (showedge === true && data.payload[i].type === "edge") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
+
       else if (showtechnical === true && data.payload[i].type === "technical") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
+
       else if (showtexture === true && data.payload[i].type === "texture") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
+
       else if (showcontrast === true && data.payload[i].type === "contrast") {
-        text += data.payload[i].name + ", " + data.payload[i].type
-        output += text + "<br>";
-      }
-
-      
-      console.log(output)
-
+        createpaintdiv(data.payload[i].name, data.payload[i].type, data.payload[i].colorGroup)}
     }
-    console.log("adding output to html")
-    resultsdiv.innerHTML = output
-
   })
   .catch(error => {
     console.error('Error:', error);
